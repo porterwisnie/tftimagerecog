4b9523f5c071c4f59b98470535606d8d23e19cb4
porterwisnie <porterwisniewski@gmail.com>
2019-07-25 16:05:57 -0700

    identify the pixels of interest

--- identify.py
+++ identify.py
@@ -1,3 +1,46 @@
-import main
+from process import loadarr, openavgs
 
+#needs to examine every pixel of every array and see if any one location has unique values for every character
+#returns locations of unique pixels
+def singleidentifier():
+    
+    poss = []
 
+    holder = []
+
+    champs = openavgs().keys()
+    #all champs images same size doesn't matter which i pick
+    for row in range(len(loadarr('aatrox'))):
+
+        for col in range(len(loadarr('aatrox')[0])):
+
+            poss.append([col,row])
+            
+            for champ in champs:
+                cpix = ''.join(str(loadarr(champ)[row][col][:3].tolist()))
+                if cpix not in holder:
+
+                    holder.append(cpix)
+                else:
+                    holder.clear()
+                    poss.pop()
+                    break
+    print(poss)
+
+#sending the index of interest responds with a dictionary of values for each character at that location
+#champion name : [red,green,blue]
+
+def allchampsatpix(col,row):
+
+    champrgb = {}
+    champs = openavgs().keys()
+
+    for champ in champs:
+
+        champrgb[champ] = loadarr(champ).tolist()[row][col][:3]
+
+    return champrgb
+
+if __name__ == "__main__":
+    
+    singleidentifier()
